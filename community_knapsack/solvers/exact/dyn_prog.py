1872206c5c482b5fba9b77dd607493b8bcd440a8
Fin Jordan <bignoldjordanf@gmail.com>
2023-03-29 02:35:02 +0100

    added multidim dynamic programming

--- community_knapsack/solvers/exact/dyn_prog.py
+++ community_knapsack/solvers/exact/dyn_prog.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, Dict
+import itertools
 
 
 def dynamic_programming(capacity: int, weights: List[int], values: List[int]) -> Tuple[List[int], int]:
@@ -132,4 +133,73 @@ def multi_dynamic_programming(
         weights: List[List[int]],
         values: List[int]
 ) -> Tuple[List[int], int]:
-    pass
+    """
+    An exact algorithm that improves upon the brute force algorithm, but is still extremely slow given larger
+    problem sizes, especially with multiple dimensions. This is very rarely applicable.
+
+    The algorithm builds up the optimal solution by iterating through all combinations of items and capacities.
+    Because we do not know the number of capacities, and we must iterate through {1,...capacity} for each capacity,
+    we generate a potentially massive product of each possible loop. We calculate the maximum possible value at
+    each sub-problem, and thus the result is stored in the sub-problem (num_items, capacities). This algorithm
+    has worst case O(n * max(capacities)^d), and cannot be better than O(n * min(capacities)^d).
+
+    :param capacities: The fixed capacities for the problem. The allocation weights cannot exceed these.
+    :param weights: A 2D list for each capacity and item, e.g., weights[j][i] is the weight of item i to capacity j.
+    :param values: A list of values for each item, i.e., values[i] is the value for item i.
+    :return: The optimal allocation for the problem as a list of project indexes and its overall value.
+    """
+    num_items: int = len(values)
+
+    # Store the maximum value achievable for any sub-problem.
+    # This approach uses a dictionary to avoid dealing with
+    # a rigid d-dimensional matrix:
+    memo: Dict[Tuple[int, ...], Tuple[List[int], int]] = {}
+
+    # Generate all possible combinations of {1, ..., num_items}
+    # and {1, ..., capacity} for each capacity, i.e., generate
+    # every possible sub-problem. This is very, very slow:
+    sets: List[List[int]] = [[i for i in range(num_items + 1)]] + \
+                            [[i for i in range(capacity + 1)] for capacity in capacities]
+
+    # We have to sort this *huge* list of sub-problems to consider
+    # increasing capacities first for each item (simulating loops):
+    sub_problems: List[Tuple[int, ...]] = sorted(list(itertools.product(*sets)))
+
+    for sub_problem in sub_problems:
+        # Split the combination into item and capacities:
+        i: int = sub_problem[0]  # Current Item
+        j: List[int] = list(sub_problem[1:])  # Current Capacities
+
+        # We have no more items or capacity to consider,
+        # so we have the empty allocation:
+        if i == 0 or any(capacity == 0 for capacity in j):
+            memo[sub_problem] = ([], 0)
+            continue
+
+        # When excluding the item, the sub-problem solution is the maximum
+        # value achievable with the first i-1 items and the same capacities:
+        exclude: Tuple[List[int], int] = memo[tuple([i - 1] + j)]
+
+        # The item must be excluded if it exceeds even one of the capacities:
+        if any(weights[cid][i - 1] > capacity for cid, capacity in enumerate(j)):
+            memo[sub_problem] = exclude
+            continue
+
+        # When including the item, the sub-problem solution is the maximum
+        # value achievable with the first i-1 items and all the capacities
+        # reduced by the current weight of the item:
+        include: Tuple[List[int], int] = memo[tuple(
+            [i - 1] + [capacity - weights[cid][i - 1] for cid, capacity in enumerate(j)]
+        )]
+        include_val: int = include[1] + values[i - 1]
+
+        # We only include the item if it can fit, otherwise we exclude it:
+        if include_val >= exclude[1]:
+            memo[sub_problem] = (include[0] + [i - 1], include_val)
+            continue
+
+        memo[sub_problem] = exclude
+
+    # The optimal solution is stored at the sub-problem where we have
+    # all the items and all the capacities available:
+    return memo[tuple([num_items] + [capacity for capacity in capacities])]
