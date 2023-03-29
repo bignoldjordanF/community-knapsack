7f9c26408777c792881a2101b333925ab24b006f
Fin Jordan <bignoldjordanf@gmail.com>
2023-03-29 18:24:26 +0100

    added greedy solvers

--- community_knapsack/solvers/approximate/__init__.py
+++ community_knapsack/solvers/approximate/__init__.py
@@ -1,3 +1,4 @@
 from .fptas import fptas
 from .genetic import genetic_algorithm, multi_genetic_algorithm
 from .sim_anneal import simulated_annealing, multi_simulated_annealing
+from .greedy import greedy, ratio_greedy, multi_greedy, multi_ratio_greedy
