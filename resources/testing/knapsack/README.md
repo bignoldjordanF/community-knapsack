71c125076a504a0894f12531212588bb46ccb6d7
Fin Jordan <bignoldjordanf@gmail.com>
2023-04-01 16:59:07 +0100

    added one-dimensional solver tests

--- resources/testing/knapsack/README.md
+++ resources/testing/knapsack/README.md
@@ -1,1 +1,2 @@
-http://people.brunel.ac.uk/~mastjjb/jeb/info.html
+* https://people.sc.fsu.edu/~jburkardt/datasets/knapsack_01/knapsack_01.html
+* http://people.brunel.ac.uk/~mastjjb/jeb/info.html
--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -3,22 +3,46 @@ from typing import List, Callable
 from community_knapsack import solvers
 
 
-instances = [
+single_instances = [
+    'resources/testing/knapsack/single1.txt',
+    'resources/testing/knapsack/single2.txt',
+    'resources/testing/knapsack/single3.txt'
+]
+single_functions = [
+    solvers.brute_force,
+    solvers.memoization,
+    solvers.dynamic_programming,
+    solvers.branch_and_bound
+]
+
+
+@pytest.mark.parametrize('instance_file_path', single_instances)
+@pytest.mark.parametrize('algorithm_fn', single_functions)
+def test_single_instance(instance_file_path: str, algorithm_fn: Callable):
+    with open(instance_file_path, 'r') as file:
+        capacity: int = int(file.readline())
+        costs: List[int] = [int(cost) for cost in file.readline().split(' ')]
+        values: List[int] = [int(value) for value in file.readline().split(' ')]
+        allocation: List[int] = [int(b) for b in file.readline().split(' ')]
+        allocation_val: int = sum(values[idx] for idx, bit in enumerate(allocation) if bit == 1)
+        assert algorithm_fn(capacity, costs, values)[1] == allocation_val
+
+
+multi_instances = [
     'resources/testing/knapsack/multi1.txt',
     'resources/testing/knapsack/multi2.txt',
     'resources/testing/knapsack/multi3.txt'
 ]
-
-functions = [
+multi_functions = [
     solvers.multi_brute_force,
     solvers.multi_memoization,
     # solvers.multi_dynamic_programming
 ]
 
 
-@pytest.mark.parametrize('instance_file_path', instances)
-@pytest.mark.parametrize('algorithm_fn', functions)
-def test_instance(instance_file_path: str, algorithm_fn: Callable):
+@pytest.mark.parametrize('instance_file_path', multi_instances)
+@pytest.mark.parametrize('algorithm_fn', multi_functions)
+def test_multi_instance(instance_file_path: str, algorithm_fn: Callable):
     with open(instance_file_path, 'r') as file:
         n, m, v = file.readline().split(' ')
         values: List[int] = [int(value) for value in file.readline().split(' ')]
