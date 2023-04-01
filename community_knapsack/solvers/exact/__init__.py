aa2e2b969023bc4dd83801ca1b9eccf2e11813c5
Fin Jordan <bignoldjordanf@gmail.com>
2023-04-01 18:14:47 +0100

    updated solver file names

--- community_knapsack/solvers/exact/__init__.py
+++ community_knapsack/solvers/exact/__init__.py
@@ -1,7 +1,7 @@
-from .brute_force import brute_force, multi_brute_force
+from .bruteforce import brute_force, multi_brute_force
 from .memoize import memoization, multi_memoization
-from .dyn_prog import dynamic_programming, \
+from .dynprog import dynamic_programming, \
     dynamic_programming_min_weight, \
     multi_dynamic_programming
-from .branch_bound import branch_and_bound, multi_branch_and_bound
+from .branchbound import branch_and_bound, multi_branch_and_bound
 from .ilp import integer_programming, multi_integer_programming
