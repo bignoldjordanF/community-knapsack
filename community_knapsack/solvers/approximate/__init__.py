749cecf10d6b73e57cd429dfe767248300616183
Fin Jordan <bignoldjordanf@gmail.com>
2023-04-02 20:25:34 +0100

    updated solver file names

--- community_knapsack/solvers/approximate/__init__.py
+++ community_knapsack/solvers/approximate/__init__.py
@@ -1,4 +1,4 @@
 from .fptas import fptas
 from .genetic import genetic_algorithm, multi_genetic_algorithm
-from .simanneal import simulated_annealing, multi_simulated_annealing
+from .sim_anneal import simulated_annealing, multi_simulated_annealing
 from .greedy import greedy, ratio_greedy, multi_greedy, multi_ratio_greedy
