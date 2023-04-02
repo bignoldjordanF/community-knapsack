99663810608e5c38304a780bc9c93a5fde0248af
Fin Jordan <bignoldjordanf@gmail.com>
2023-04-02 20:07:49 +0100

    begin refactor

--- community_knapsack/__init__.py
+++
@@ -1,3 +0,0 @@
-from .pbproblem import PBProblem, PBMultiProblem, PBAlgorithm, PBMultiAlgorithm, PBResult
-from .pbgenerator import PBGenerator, PBMultiGenerator
-from .pbfileservice import PBParser, PBWriter
