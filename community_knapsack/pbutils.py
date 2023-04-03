625301351680b6de8d3a2716b56201e542bb7f0c
Fin Jordan <bignoldjordanf@gmail.com>
2023-04-03 01:50:07 +0100

    added utility functions for converting votes

---
+++ community_knapsack/pbutils.py
@@ -0,0 +1,122 @@
+from typing import List
+
+
+def ordinal_to_utility(num_projects: int, votes: List[int], min_vote_length: int = -1, max_vote_length: int = -1):
+    """
+    Converts a list of ordinal preferences `votes` ordered from most to least preferred into a utility
+    vector or list over the projects. This function should be called on a voter-by-voter basis, where
+    votes contains the project *indexes* of the projects the voter 'prefers'. The min- and max-vote-
+    length parameters are optional, but if they are equal then the Borda count algorithm is used, or
+    otherwise the utilities are dictated by the number of preferences (projects) submitted.
+
+    :param num_projects: The number of projects that votes was picked from, such that len(votes) <= num_projects.
+    :param votes: A list of project *indexes* (not ids) that the voter `prefers`, s.t. len(votes) <= num_projects.
+    :param min_vote_length: (Optional) The minimum number of preferences (projects) that the voter can submit.
+    :param max_vote_length: (Optional) The maximum number of preferences (projects) that the voter can submit.
+    :return: A `num_projects`-length list of utilities over all the projects, i.e., zero for all those not in votes.
+    """
+    # The votes list must only contain project ids:
+    if num_projects < len(votes) or any(vote >= num_projects for vote in votes):
+        raise ValueError('The vote list must only contain project indexes (not ids), and so there should '
+                         'be no more than `num_projects` votes, and each project id should be < `num_projects`.')
+
+    # The votes list must not contain duplicate projects:
+    if len(set(votes)) != len(votes):
+        raise ValueError('A single project was voted for more than once by a voter.')
+
+    if 0 <= min_vote_length > len(votes):
+        raise ValueError('There can at most `min_vote_length` projects submitted when enabling minimum vote length.')
+
+    if 0 <= max_vote_length < len(votes):
+        raise ValueError('There can at most `max_vote_length` projects submitted when enabling maximum vote length.')
+
+    # Update Minimum & Maximum Vote Length
+    if min_vote_length == -1:
+        min_vote_length = 0
+
+    if max_vote_length == -1:
+        max_vote_length = num_projects
+
+    utility: List[int] = [0] * num_projects
+
+    # Use the Borda count if there is a limit on minimum and maximum vote length:
+    if min_vote_length >= 0 and min_vote_length == max_vote_length:
+        count: int = 1
+        for preference in reversed(votes):
+            utility[preference] = count
+            count += 1
+        return utility
+
+    # TODO: Verify or prove this function
+    # Otherwise, simulate cumulative voting by finding some score to distribute
+    # over the projects based on the preferences not submitted:
+    max_utility: int = min(num_projects, max_vote_length)
+    not_submitted: int = max_utility - len(votes)
+    current_utility: int = sum(k for k in range(1, not_submitted+1)) + 1
+
+    for preference in reversed(votes):
+        utility[preference] = current_utility
+        current_utility += 1
+
+    return utility
+
+
+def vote_to_utility(num_projects: int, vote_type: str, votes: List[int], points: List[int] = None):
+    """
+    Converts a list of votes (and points in cumulative and scoring voting) into a utility vector or list
+    over the projects. This function should be called on a voter-by-voter basis, where votes contains
+    the project *indexes* of the projects the voter 'likes', and points is the corresponding number of
+    votes/points/utility the voter gives to each project. That is, if votes[i]=3, then points[3] is
+    the number of points the voter gives to project 3.
+
+    Approval: The `votes` list contains the projects the voter approves.
+    Cumulative: The voter has `x` votes to give to each project, denoted by the `votes` and `points` lists.
+    Scoring: The voter scores each project, denoted by the `votes` and `points` lists.
+    Ordinal: The voter submits preferences, where `votes` is in order from most to least preferred.
+
+    :param num_projects: The number of projects that votes was picked from, such that len(votes) <= num_projects.
+    :param vote_type: The type of voting method used, i.e., 'approval', 'cumulative', 'scoring' or 'ordinal'.
+    :param votes: A list of project *indexes* (not ids) that the voter has `selected`, s.t. len(votes) <= num_projects.
+    :param points: A list of corresponding points to each project id in vote (only in cumulative and scoring voting).
+    :return: A `num_projects`-length list of utilities over all the projects, i.e., zero for all those not in votes.
+    """
+    # The vote type must be one of approval, cumulative, scoring or ordinal:
+    if vote_type not in ('approval', 'cumulative', 'scoring', 'ordinal'):
+        raise ValueError('The vote type must be one of "approval", "cumulative", "scoring" or "ordinal".')
+
+    # The votes list must only contain project ids:
+    if num_projects < len(votes) or any(vote >= num_projects for vote in votes):
+        raise ValueError('The vote list must only contain project indexes (not ids), and so there should '
+                         'be no more than `num_projects` votes, and each project id should be < `num_projects`.')
+
+    # The votes list must not contain duplicate projects:
+    if len(set(votes)) != len(votes):
+        raise ValueError('A single project was voted for more than once by a voter.')
+
+    if vote_type in ('cumulative', 'scoring'):
+        # The points list must be the same length as votes for cumulative and scoring voting:
+        if points is None or len(points) != len(votes):
+            if points is None:
+                points = []
+            raise ValueError(f'There must be as many points ({len(points)} found) as votes ({len(votes)} found) in'
+                             f'cumulative or scoring voting.')
+
+    utility: List[int] = [0] * num_projects
+
+    # Approval voting gives all projects in `votes` a utility of one:
+    if vote_type == 'approval':
+        for vote in votes:
+            utility[vote] = 1
+        return utility
+
+    # Cumulative and scoring gives projects in `votes` their points:
+    if vote_type in ('cumulative', 'scoring'):
+        for idx, vote in enumerate(votes):
+            utility[vote] = points[idx]
+        return utility
+
+    # Ordinal voting uses a different strategy:
+    if vote_type in 'ordinal':
+        return ordinal_to_utility(num_projects, votes)
+
+    return utility
---
+++ tests/test_pb_utils.py
@@ -0,0 +1,113 @@
+from community_knapsack import pbutils
+import pytest
+
+
+class TestPBUtils:
+    """Ensures the utility functions return the correct results and raise helpful errors
+    and warnings when the input is incorrect."""
+
+    def test_bad_vote_type(self):
+        """Ensures that the vote type must be one of approval, cumulative, scoring or ordinal."""
+        with pytest.raises(ValueError):
+            pbutils.vote_to_utility(num_projects=5, vote_type='what?', votes=[], points=[])
+
+    def test_too_many_votes(self):
+        """Ensures that there is at most `num_projects` votes in the votes list."""
+        with pytest.raises(ValueError):
+            pbutils.vote_to_utility(num_projects=5, vote_type='approval', votes=[1, 2, 3, 4, 5, 6], points=[])
+
+    def test_bad_project_vote(self):
+        """Ensures that there are only project indexes in the votes list."""
+        with pytest.raises(ValueError):
+            pbutils.vote_to_utility(num_projects=5, vote_type='approval', votes=[1, 2, 354, 4, 5, 6], points=[])
+
+    def test_duplicate_vote(self):
+        """Ensures that each project is voted for at most once by a voter."""
+        with pytest.raises(ValueError):
+            pbutils.vote_to_utility(num_projects=5, vote_type='approval', votes=[2, 2], points=[])
+
+    def test_missing_points(self):
+        """Ensures that there are only project indexes in the votes list."""
+        with pytest.raises(ValueError):
+            pbutils.vote_to_utility(num_projects=5, vote_type='cumulative', votes=[0, 1, 2, 3, 4])
+            pbutils.vote_to_utility(num_projects=5, vote_type='scoring', votes=[0, 1, 2, 3, 4], points=[2])
+
+    def test_ordinal_too_many_votes(self):
+        """Ensures that there is at most `num_projects` votes in the votes list."""
+        with pytest.raises(ValueError):
+            pbutils.ordinal_to_utility(num_projects=5, votes=[1, 2, 3, 4, 5])
+
+    def test_ordinal_bad_project_vote(self):
+        """Ensures that there are only project indexes in the votes list."""
+        with pytest.raises(ValueError):
+            pbutils.ordinal_to_utility(num_projects=5, votes=[1, 2, 354, 4, 5, 6])
+
+    def test_ordinal_min_vote_length(self):
+        """Ensures that there are at most min_vote_length preferences submitted."""
+        with pytest.raises(ValueError):
+            pbutils.ordinal_to_utility(num_projects=5, votes=[], min_vote_length=1)
+
+    def test_ordinal_max_vote_length(self):
+        """Ensures that there are at most min_vote_length preferences submitted."""
+        with pytest.raises(ValueError):
+            pbutils.ordinal_to_utility(num_projects=5, votes=[1, 2], max_vote_length=1)
+
+    def test_empty_success(self):
+        """Ensures that there are at most min_vote_length preferences submitted."""
+        assert pbutils.vote_to_utility(
+            num_projects=0,
+            vote_type='cumulative',
+            votes=[],
+            points=[]
+        ) == []
+
+    def test_approval_voting_success(self):
+        """Ensures that there are at most min_vote_length preferences submitted."""
+        assert pbutils.vote_to_utility(
+            num_projects=5,
+            vote_type='approval',
+            votes=[0, 1, 4]
+        ) == [1, 1, 0, 0, 1]
+
+    def test_cumulative_voting_success(self):
+        """Ensures that there are at most min_vote_length preferences submitted."""
+        assert pbutils.vote_to_utility(
+            num_projects=5,
+            vote_type='cumulative',
+            votes=[2, 4],
+            points=[10, 11]
+        ) == [0, 0, 10, 0, 11]
+
+    def test_scoring_voting_success(self):
+        """Ensures that there are at most min_vote_length preferences submitted."""
+        assert pbutils.vote_to_utility(
+            num_projects=5,
+            vote_type='scoring',
+            votes=[2, 3],
+            points=[10, 11]
+        ) == [0, 0, 10, 11, 0]
+
+    def test_ordinal_voting_success(self):
+        """Ensures that there are at most min_vote_length preferences submitted."""
+        assert pbutils.vote_to_utility(
+            num_projects=5,
+            vote_type='ordinal',
+            votes=[2, 3]
+        ) == [0, 0, 8, 7, 0]
+
+    def test_ordinal_voting_success_2(self):
+        """Ensures that there are at most min_vote_length preferences submitted."""
+        assert pbutils.vote_to_utility(
+            num_projects=5,
+            vote_type='ordinal',
+            votes=[2, 3, 1]
+        ) == [0, 4, 6, 5, 0]
+
+    def test_borda_count(self):
+        """Ensures that there are at most min_vote_length preferences submitted."""
+        assert pbutils.ordinal_to_utility(
+            num_projects=5,
+            votes=[2, 3, 1],
+            min_vote_length=3,
+            max_vote_length=3
+        ) == [0, 1, 3, 2, 0]
