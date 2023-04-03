6713a61dddfd6408d377619eadae2bb238a977fc
Fin Jordan <bignoldjordanf@gmail.com>
2023-04-03 04:10:57 +0100

    added documentation

--- community_knapsack/pbresult.py
+++ community_knapsack/pbresult.py
@@ -5,7 +5,16 @@ from typing import List
 @dataclass
 class PBResult:
     allocation: List[int]
+    """A list of project ids (not indexes) that are funded in the budget allocation."""
+
     value: int
+    """The overall value (e.g., number of votes or points in utilitarian) of the allocation -- higher is better."""
+
     runtime: float
+    """The overall run-time (i.e. time in milliseconds) of the algorithm."""
+
     algorithm: str
+    """The algorithm used to solve the problem."""
+
     approximate: bool
+    """True if the allocation found is an approximation or if it is exactly correct."""
