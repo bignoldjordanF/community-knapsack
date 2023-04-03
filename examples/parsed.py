8486275b4dbdda8f6d19686b9502c5177aa7f060
Fin Jordan <bignoldjordanf@gmail.com>
2023-04-03 11:52:07 +0100

    updated parsed example

--- examples/parsed.py
+++ examples/parsed.py
@@ -4,6 +4,5 @@ from community_knapsack import PBParser, PBSingleProblem, PBSingleAlgorithm, pbu
 if __name__ == '__main__':
     parser: PBParser = PBParser('../resources/pabulib/small/poland_warszawa_2017_aleksandrow.pb')
     problem: PBSingleProblem = parser.problem()
-    print(pbutils.aggregate_utilitarian(problem.num_projects, problem.utilities))
     print(problem.solve(PBSingleAlgorithm.BRANCH_AND_BOUND))
     print(problem.solve(PBSingleAlgorithm.GREEDY))
