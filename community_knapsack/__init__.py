b57a8b51b5dbd5c0b419e762a94cc363de0c12c2
Fin Jordan <bignoldjordanf@gmail.com>
2023-04-03 11:51:47 +0100

    added pbresult

--- community_knapsack/__init__.py
+++ community_knapsack/__init__.py
@@ -2,5 +2,6 @@ from .pbproblem import PBSingleProblem,\
     PBMultiProblem
 from .pbalgorithm import PBSingleAlgorithm, \
     PBMultiAlgorithm
+from .pbresult import PBResult
 from .io import PBParser
 from . import solvers
