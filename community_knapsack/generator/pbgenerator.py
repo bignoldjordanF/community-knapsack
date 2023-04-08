772371fe38337e3d27817854f3a53cf04b336da8
Fin Jordan <bignoldjordanf@gmail.com>
2023-04-08 13:46:14 +0100

    updated utility generation function

--- community_knapsack/generator/pbgenerator.py
+++ community_knapsack/generator/pbgenerator.py
@@ -1,6 +1,7 @@
 from community_knapsack import PBSingleProblem, PBMultiProblem
 from typing import Sequence, Tuple, List
 from random import Random
+import warnings
 
 
 class PBGenerator:
@@ -39,6 +40,7 @@ class PBGenerator:
             num_projects: int,
             num_voters: int,
             utility_bound: Tuple[int, int],
+            vote_length_bound: Tuple[int, int] = (0, -1)
     ) -> List[List[int]]:
         """
         Generates a random 2-dimensional list of utilities for each voter over each project. Each
@@ -47,18 +49,39 @@ class PBGenerator:
         :param num_projects: The number of projects to generate utilities for.
         :param num_voters: The number of voters to generate utilities for.
         :param utility_bound: The bounds of possible utilities as a tuple (min_bound, max_bound).
+        :param vote_length_bound: The minimum and maximum number of votes each voter can make.
         :return: A two-dimensional list of utilities for each voter over each project.
         """
 
-        weightings: List[float] = [self._random.random() for _ in range(num_projects)]
-        if utility_bound[0] == 0:
+        if vote_length_bound[0] < 0:
+            vote_length_bound = (0, vote_length_bound[1])
+
+        if vote_length_bound[1] < 0:
+            vote_length_bound = (vote_length_bound[0], num_projects)
+
+        if utility_bound[0] <= 0:
             utility_bound = (1, utility_bound[1])
-        return [
-            [
-                self._generate_int(utility_bound) if self._random.random() < weightings[idx]**2 else 0
-                for idx in range(num_projects)
-            ] for _ in range(num_voters)
-        ]
+
+        weightings: List[float] = [min(max(self._random.gauss(0.5, 0.2), 0), 1) for _ in range(num_projects)]
+        num_votes: int = self._generate_int(vote_length_bound)
+
+        utilities: List[List[int]] = [[0 for _ in range(num_projects)] for _ in range(num_voters)]
+
+        for voter in range(num_voters):
+            for selection in self._random.choices(range(num_projects), weightings, k=num_votes):
+                utilities[voter][selection] = self._generate_int(utility_bound)
+
+        return utilities
+
+        # weightings: List[float] = [self._random.random() for _ in range(num_projects)]
+        # if utility_bound[0] == 0:
+        #     utility_bound = (1, utility_bound[1])
+        # return [
+        #     [
+        #         self._generate_int(utility_bound) if self._random.random() < weightings[idx]**2 else 0
+        #         for idx in range(num_projects)
+        #     ] for _ in range(num_voters)
+        # ]
 
         # return [[self._generate_int(utility_bound) for _ in range(num_projects)] for _ in range(num_voters)]
 
@@ -69,6 +92,7 @@ class PBGenerator:
             budget_bound: Tuple[int, int],
             cost_bound: Tuple[int, int],
             utility_bound: Tuple[int, int] = (0, 1),
+            vote_length_bound: Tuple[int, int] = (0, -1)
     ) -> PBSingleProblem:
         """
         Creates a PBSingleProblem object containing randomly generated instance data, i.e., a random
@@ -80,6 +104,7 @@ class PBGenerator:
         :param budget_bound: The minimum and maximum possible budget.
         :param cost_bound: The minimum and maximum cost for each project.
         :param utility_bound: The minimum and maximum utility that each voter derives from each project.
+        :param vote_length_bound: The minimum and maximum number of votes each voter can make.
         :return: A PBSingleProblem object containing the randomly generated instance.
         """
         num_projects: int = self._generate_int(num_projects_bound)
@@ -93,7 +118,8 @@ class PBGenerator:
         utilities: List[List[int]] = self._generate_utilities(
             num_projects,
             num_voters,
-            utility_bound
+            utility_bound,
+            vote_length_bound
         )
 
         return PBSingleProblem(
@@ -111,6 +137,7 @@ class PBGenerator:
             budget_bound: Sequence[Tuple[int, int]],
             cost_bound: Sequence[Tuple[int, int]],
             utility_bound: Tuple[int, int] = (0, 1),
+            vote_length_bound: Tuple[int, int] = (0, -1)
     ) -> PBMultiProblem:
         """
         Creates a PBMultiProblem object containing randomly generated instance data, i.e., a random
@@ -122,6 +149,7 @@ class PBGenerator:
         :param budget_bound: A sequence of minimum and maximum bounds for each budget.
         :param cost_bound: A sequence of minimum and maximum bounds for each project cost for each budget.
         :param utility_bound: The minimum and maximum utility that each voter derives from each project.
+        :param vote_length_bound: The minimum and maximum number of votes each voter can make.
         :return: A PBMultiProblem object containing the randomly generated instance.
         """
         num_projects: int = self._generate_int(num_projects_bound)
@@ -142,7 +170,8 @@ class PBGenerator:
         utilities: List[List[int]] = self._generate_utilities(
             num_projects,
             num_voters,
-            utility_bound
+            utility_bound,
+            vote_length_bound
         )
 
         return PBMultiProblem(
