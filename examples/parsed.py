2acc580f46bc3ebe9682a59437844c8d182b34fe
Fin Jordan <bignoldjordanf@gmail.com>
2023-04-15 23:27:15 +0100

    backup (computer is breaking)

--- examples/parsed.py
+++ examples/parsed.py
@@ -4,5 +4,5 @@ from community_knapsack import PBParser, PBSingleProblem, PBSingleAlgorithm
 if __name__ == '__main__':
     parser: PBParser = PBParser('../resources/pabulib/small/poland_warszawa_2017_aleksandrow.pb')
     problem: PBSingleProblem = parser.single_problem()
-    print(problem.solve(PBSingleAlgorithm.BRANCH_AND_BOUND))
+    print(problem.solve(PBSingleAlgorithm.BRANCH_AND_BOUND, timeout=300))
     print(problem.solve(PBSingleAlgorithm.GREEDY))
