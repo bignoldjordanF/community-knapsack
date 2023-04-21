f4dfedd619978246646acd5fb13cdbdd1329f766
bignoldjordanF <bignoldjordanf@gmail.com>
2023-04-21 22:36:57 +0100

    updated parsed example file

--- examples/parsed.py
+++ examples/parsed.py
@@ -2,7 +2,8 @@ from community_knapsack import PBParser, PBSingleProblem, PBSingleAlgorithm
 
 
 if __name__ == '__main__':
-    parser: PBParser = PBParser('../resources/pabulib/small/poland_warszawa_2017_aleksandrow.pb')
+    parser: PBParser = PBParser('../resources/pabulib/poland_warszawa_2017_aleksandrow.pb')
     problem: PBSingleProblem = parser.single_problem()
     print(problem.solve(PBSingleAlgorithm.BRANCH_AND_BOUND))
+    print(problem.solve(PBSingleAlgorithm.ILP_SOLVER))
     print(problem.solve(PBSingleAlgorithm.GREEDY))
