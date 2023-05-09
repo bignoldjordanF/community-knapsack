037bae7ee1ee473d3b3d342e788d1bb18b9d4c4a
bignoldjordanF <bignoldjordanf@gmail.com>
2023-05-09 15:02:15 +0100

    changed parsed file

--- examples/parsed.py
+++ examples/parsed.py
@@ -2,6 +2,6 @@ from community_knapsack import PBParser, PBSingleProblem, PBSingleAlgorithm
 
 
 if __name__ == '__main__':
-    parser: PBParser = PBParser('../resources/pabulib/poland_warszawa_2017_aleksandrow.pb')
+    parser: PBParser = PBParser('../resources/pabulib/poland_warszawa_2019_targowek-fabryczny-elsnerow-i-utrata.pb')
     problem: PBSingleProblem = parser.single_problem()
-    print(problem.solve(PBSingleAlgorithm.BRANCH_AND_BOUND))
+    print(problem.solve(PBSingleAlgorithm.DYNAMIC_PROGRAMMING))
